bbd38e1cffb766005e809f125264cd212450cefc
root <root@cld-config.cloud.pd.infn.it>
2021-12-13 09:34:25 +0100

    New path for bacula rpms

--- compute_xena/manifests/bacula.pp
+++ compute_xena/manifests/bacula.pp
@@ -23,13 +23,13 @@ case $operatingsystemrelease {
               package {"bareos-common":
                  provider => "rpm",
                  ensure   => installed,
-                 source => "http://download.bareos.org/bareos/release/latest/CentOS_8/x86_64/bareos-common-19.2.7-2.el8.x86_64.rpm",
+                 source => "https://download.bareos.org/bareos/release/19.2/CentOS_8/x86_64/bareos-common-19.2.7-2.el8.x86_64.rpm",
                       }  
  
               package {"bareos-filedaemon":
                  provider => "rpm",
                  ensure   => installed,
-                 source => "http://download.bareos.org/bareos/release/latest/CentOS_8/x86_64/bareos-filedaemon-19.2.7-2.el8.x86_64.rpm",
+                 source => "https://download.bareos.org/bareos/release/19.2/CentOS_8/x86_64/bareos-filedaemon-19.2.7-2.el8.x86_64.rpm",
                  require => Package["bareos-common"],
                       }  
   
